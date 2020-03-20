c02a7d64d84c28ce51d8fee88112d089efc121b5
Mohit Goel <55006916+MohitGoel92@users.noreply.github.com>
2020-03-20 23:31:21 +0000

    Update Thompson_Sampling.py

--- Thompson_Sampling.py
+++ Thompson_Sampling.py
@@ -11,7 +11,7 @@ import pandas as pd
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
 # Random Selection
-# After running this model numerous times, we see that the total rewards is usually between 1200 and 1300
+# After running this model numerous times, we see that the total rewards is usually between 1200 and 1300.
 
 # Implementing Random Selection
 
@@ -31,7 +31,7 @@ for n in range(0, N):
 
 # Visualising the results
 
-# As expected, we see that each ad was selected approximately equally
+# As expected, we more or less see an even distribution of the ads selected
 
 plt.hist(ads_selected)
 plt.title('Histogram of ads selections')
@@ -39,8 +39,8 @@ plt.xlabel('Ads')
 plt.ylabel('Number of times each ad was selected')
 plt.show()
 
-# We observe that advert 5 has the highest total rewards, threfore we conclude that advert 5 is the best as it 
-# received the most clicks
+# We observe that advert 5 has the highest total rewards, therefore we conclude that advert 5 is the best as it 
+# received the most clicks.
 
 plt.bar(range(1,11), sums_of_rewards, color = 'green')
 plt.title('Bar chart of rewards per advert')
@@ -54,7 +54,7 @@ plt.show()
 # =============================================================================
 
 # Implementing Thompson Sampling
-# Using the Thompon Sampling Algorithm, we see that the total reward usually around 2500. Unlike the UCB 
+# Using the Thompson Sampling Algorithm, we see that the total reward is usually around 2500. Unlike the UCB 
 # algorithm Thompson Sampling is probabilistic, resulting in a degree of randomness being present.
 # We conclude that due to the Thompson Sampling Algorithm, we have recieved approximately double the 
 # rewards as opposed to the random selection method, as a result of exploiting of the optimal advert.
@@ -84,8 +84,8 @@ for n in range(0, N):
 
 # Visualising the results - Histogram
     
-# Both charts resemble the same shape as the more the most popular advert is selected the more it will run,
-# hence having the opportunity to receive a reward.
+# Both charts resemble the same shape due to the more popular advert being selected more often, resulting in
+# receiving more rewards.
 
 plt.hist(ads_selected)
 plt.title('Histogram of ads selections')
