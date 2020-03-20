f75d60303001b10cc64e7de98785b3f1215ccabb
Mohit Goel <55006916+MohitGoel92@users.noreply.github.com>
2020-03-20 23:27:18 +0000

    Add files via upload

---
+++ Thompson_Sampling.py
@@ -0,0 +1,101 @@
+# Reinforcement Learning - Thompson Sampling
+
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+# Random Selection
+# After running this model numerous times, we see that the total rewards is usually between 1200 and 1300
+
+# Implementing Random Selection
+
+import random
+N = 10000
+d = 10
+ads_selected = []
+sums_of_rewards = [0] * d
+total_reward = 0
+
+for n in range(0, N):
+    ad = random.randrange(d)
+    ads_selected.append(ad)
+    reward = dataset.values[n, ad]
+    sums_of_rewards[ad] = sums_of_rewards[ad] + reward
+    total_reward = total_reward + reward
+
+# Visualising the results
+
+# As expected, we see that each ad was selected approximately equally
+
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
+
+# We observe that advert 5 has the highest total rewards, threfore we conclude that advert 5 is the best as it 
+# received the most clicks
+
+plt.bar(range(1,11), sums_of_rewards, color = 'green')
+plt.title('Bar chart of rewards per advert')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was rewarded')
+plt.show()
+
+'''
+# =============================================================================
+# 
+# =============================================================================
+
+# Implementing Thompson Sampling
+# Using the Thompon Sampling Algorithm, we see that the total reward usually around 2500. Unlike the UCB 
+# algorithm Thompson Sampling is probabilistic, resulting in a degree of randomness being present.
+# We conclude that due to the Thompson Sampling Algorithm, we have recieved approximately double the 
+# rewards as opposed to the random selection method, as a result of exploiting of the optimal advert.
+
+N = 10000
+d = 10
+ads_selected = []
+numbers_of_rewards_1 = [0] * d
+numbers_of_rewards_0 = [0] * d
+total_reward = 0
+
+for n in range(0, N):
+    ad = 0
+    max_random = 0
+    for i in range(0, d):
+        random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+        if random_beta > max_random:
+            max_random = random_beta
+            ad = i
+    ads_selected.append(ad)
+    reward = dataset.values[n, ad]
+    if reward == 1:
+        numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1
+    else:
+        numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1
+    total_reward = total_reward + reward
+
+# Visualising the results - Histogram
+    
+# Both charts resemble the same shape as the more the most popular advert is selected the more it will run,
+# hence having the opportunity to receive a reward.
+
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show() 
+
+plt.bar(range(1,11), numbers_of_rewards_1, color = 'grey')
+plt.title('Bar chart of ads rewarded')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was rewarded')
+plt.show()
+'''
